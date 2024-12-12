cbb0387408a14b0ca20971c57c9a6152715053f3
roguelogan10 <fabriceessinam.paka@gmail.com>
2024-12-12 02:01:38 +0100

    Ajout d'un nouveau programme python

---
+++ python_standard/Hello_nom.py
@@ -0,0 +1,16 @@
+"""
+Program Hello nom
+
+    Objectif: 
+        affiche: "Hello Fabrice!" si l'utilisateur lui fournit 'Fabrice' comme nom.
+
+    Realisation:
+        Date                                      Programmeur     
+        ====                                      ============ 
+        12/12/2024                                PAKA Fabrice Essinam
+"""
+nom = input('Nom:')
+print('Hello '+ nom +' !')
+
+
+
