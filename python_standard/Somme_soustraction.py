34cf2bc68643450bcb3f632b0787a16126f2c272
roguelogan10 <fabriceessinam.paka@gmail.com>
2024-12-12 01:50:36 +0100

    Ajout de nouveau dossier et de quelques programmes python

---
+++ python_standard/Somme_soustraction.py
@@ -0,0 +1,21 @@
+"""
+Program Somme et soustraction
+
+    Objectif: 
+        calcul et affiche la somme et la soustraction de deux entiers 
+        fournis par l'utilisateur.
+
+    Realisation:
+        Date                                      Programmeur     
+        ====                                      ============ 
+        12/12/2024                                PAKA Fabrice Essinam
+"""
+    
+entier_1 = int(input('Premier entier:'))
+
+entier_2 = int(input('Deuxième entier:'))
+
+print(f"Somme:{entier_1+entier_2}.",end="\n")
+
+print(f"Différence:{entier_1-entier_2}.")
+
---
+++ python_standard/euro_en_dollar.py
@@ -0,0 +1,14 @@
+"""
+Program Euro en dollar
+
+    Objectif: 
+        Convertit ,toute somme en euro, fournit par l'utilisateur en dollar
+
+    Realisation:
+        Date                                      Programmeur     
+        ====                                      ============ 
+        12/12/2024                                PAKA Fabrice Essinam
+"""
+eur = float(input('Prix en euros:'))
+
+print('Prix en dollars:',eur*1.0627)
